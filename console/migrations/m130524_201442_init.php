282cf5a4efb634e1f3c7921cf589c1ef247145ea
yusufjonc07 <ahmedovyusufjon3@gmail.com>
2024-12-23 16:16:44 +0900

    Project setup

---
+++ console/migrations/m130524_201442_init.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\db\Migration;
+
+class m130524_201442_init extends Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // https://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%user}}', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string()->notNull()->unique(),
+            'phone' => $this->string()->unique(),
+            'purpose' => $this->string()->notNull(),
+            'business_type' => $this->string(),
+            'business_desciption' => $this->string(),
+            'company_scale' => $this->string(),
+            'auth_key' => $this->string(32)->notNull(),
+            'password_hash' => $this->string()->notNull(),
+            'password_reset_token' => $this->string()->unique(),
+            'email' => $this->string()->notNull()->unique(),
+            'status' => $this->smallInteger()->notNull()->defaultValue(10),
+            'created_at' => $this->timestamp()->defaultExpression('NOW()'),
+            'updated_at' => $this->timestamp()->defaultExpression('NOW()'),
+        ], $tableOptions);
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
