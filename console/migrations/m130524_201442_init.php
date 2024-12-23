528a679a0ad4cabdde5894528af44f7f630f4de7
yusufjonc07 <ahmedovyusufjon3@gmail.com>
2024-12-23 16:25:48 +0900

    Update user migration

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -27,6 +27,7 @@ class m130524_201442_init extends Migration
             'status' => $this->smallInteger()->notNull()->defaultValue(10),
             'created_at' => $this->timestamp()->defaultExpression('NOW()'),
             'updated_at' => $this->timestamp()->defaultExpression('NOW()'),
+            
         ], $tableOptions);
     }
 
