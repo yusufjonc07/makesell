078718fefc8d8c9be9f9f79b8aa45a03c4fbf8c0
yusufjonc07 <ahmedovyusufjon3@gmail.com>
2025-01-09 16:58:39 +0900

    Add footer

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -44,12 +44,7 @@ AppAsset::register($this);
 
     <?= $content ?>
 
-    <footer class="footer mt-auto py-3 text-muted">
-        <div class="container">
-            <p class="float-start">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
-            <p class="float-end"></p>
-        </div>
-    </footer>
+    <?= $this->render('/shared/_footer') ?>
 
     <?php $this->endBody() ?>
 </body>
---
+++ frontend/views/shared/_footer.php
@@ -0,0 +1,152 @@
+<footer id="footer" class="py-5 border-top">
+    <div class="container-lg">
+      <div class="row">
+        <div class="col-lg-2 pb-3">
+          <div class="footer-menu">
+            <h5 class="widget-title pb-2">Info</h5>
+            <ul class="menu-list list-unstyled">
+              <li class="pb-2">
+                <a href="#">Track Your Order</a>
+              </li>
+              <li class="pb-2">
+                <a href="index.html">Our Blog</a>
+              </li>
+              <li class="pb-2">
+                <a href="#">Privacy policy</a>
+              </li>
+              <li class="pb-2">
+                <a href="#">Shipping</a>
+              </li>
+              <li class="pb-2">
+                <a href="#">Contact Us</a>
+              </li>
+              <li class="pb-2">
+                <a href="#">Help</a>
+              </li>
+              <li class="pb-2">
+                <a href="#">Community</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-2 pb-3">
+          <div class="footer-menu">
+            <h5 class="widget-title pb-2">About</h5>
+            <ul class="menu-list list-unstyled">
+              <li class="pb-2">
+                <a href="#">History</a>
+              </li>
+              <li class="pb-2">
+                <a href="#">Our Team</a>
+              </li>
+              <li class="pb-2">
+                <a href="#">Services</a>
+              </li>
+              <li class="pb-2">
+                <a href="#">Company</a>
+              </li>
+              <li class="pb-2">
+                <a href="#">Manufacture</a>
+              </li>
+              <li class="pb-2">
+                <a href="#">Wholesale</a>
+              </li>
+              <li class="pb-2">
+                <a href="#">Retail</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-2 pb-3">
+          <div class="footer-menu">
+            <h5 class="widget-title pb-2">Women Shoes</h5>
+            <ul class="menu-list list-unstyled">
+              <li class="pb-2">
+                <a href="#">Track Your Order</a>
+              </li>
+              <li class="pb-2">
+                <a href="index.html">Our Blog</a>
+              </li>
+              <li class="pb-2">
+                <a href="#">Privacy policy</a>
+              </li>
+              <li class="pb-2">
+                <a href="#">Shipping</a>
+              </li>
+              <li class="pb-2">
+                <a href="#">Contact Us</a>
+              </li>
+              <li class="pb-2">
+                <a href="#">Help</a>
+              </li>
+              <li class="pb-2">
+                <a href="#">Community</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-2 pb-3">
+          <div class="footer-menu">
+            <h5 class="widget-title pb-2">Popular</h5>
+            <ul class="menu-list list-unstyled">
+              <li class="pb-2">
+                <a href="#">Prices Drop</a>
+              </li>
+              <li class="pb-2">
+                <a href="#">New Products</a>
+              </li>
+              <li class="pb-2">
+                <a href="#">Best Sales</a>
+              </li>
+              <li class="pb-2">
+                <a href="index.html">Stores</a>
+              </li>
+              <li class="pb-2">
+                <a href="#" data-bs-toggle="modal" data-bs-target="#modallogin">Login</a>
+              </li>
+              <li class="pb-2">
+                <a href="#" data-bs-toggle="modal" data-bs-target="#modallong">Cart</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-2 pb-3">
+          <div class="footer-menu">
+            <h5 class="widget-title pb-2">Mens Collection</h5>
+            <ul class="menu-list list-unstyled">
+              <li class="pb-2">
+                <a href="#">Delivery</a>
+              </li>
+              <li class="pb-2">
+                <a href="index.html">About Us</a>
+              </li>
+              <li class="pb-2">
+                <a href="#">Shoes</a>
+              </li>
+              <li class="pb-2">
+                <a href="#">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-2 pb-3">
+          <div class="footer-menu">
+            <h5 class="widget-title pb-3">Get In Touch</h5>
+            <div class="footer-contact-text">
+              <span>Stylish Online Store 123 Main Street, Toulouse - France. </span>
+              <span> Call us: (+33) 800 456 789-987 </span>
+              <span class="text-hover fw-bold light-border"><a href="mailto:contact@yourwebsite.com">contact@yourwebsite.com</a></span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <p>© Copyright Stylish 2023.</p>
+        </div>
+        <div class="col-md-6 text-lg-end">
+          <p>Free HTML by <a href="https://templatesjungle.com/" target="_blank">TemplatesJungle</a><br> Distributed by <a href="https://themewagon.com" target="blank">ThemeWagon</a> </p>
+        </div>
+      </div>
+    </div>
+  </footer>
