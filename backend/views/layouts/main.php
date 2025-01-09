8970c27be5dd73695295e816801eea21d5585201
yusufjonc07 <ahmedovyusufjon3@gmail.com>
2025-01-10 02:30:25 +0900

    .

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -44,7 +44,6 @@ AppAsset::register($this);
         $menuItems = [
             ['label' => 'Commerce', 'url' => ['/customer/index']],
             ['label' => 'Stock', 'url' => ['/stock/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
         ];
         if (Yii::$app->user->isGuest) {
             $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
